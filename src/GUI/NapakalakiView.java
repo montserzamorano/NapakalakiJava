28c673973f8375055099b411c468a812ac9261e3
montse <montse@192.168.1.104>
2015-07-25 00:05:22 +0200

    Hasta incluir vistas. Error en incluir vista contenida.

--- src/GUI/NapakalakiView.java
+++ src/GUI/NapakalakiView.java
@@ -1,16 +1,25 @@
 package GUI;
+import Model.*;
 
 /**
  *
- * @author montse
+ * @author Montse R.Zamorano
  */
 public class NapakalakiView extends javax.swing.JFrame {
+    private Napakalaki napakalakiModel;
 
     /**
      * Creates new form NapakalakiView4
      */
     public NapakalakiView() {
         initComponents();
+    }
+    
+    /**
+     * @brief método para darle valor al atributo napakalakiModel
+     */
+    public void setNapakalaki(Napakalaki valor){
+        this.napakalakiModel = valor;
     }
 
     /**
@@ -22,17 +31,49 @@ public class NapakalakiView extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        meet = new javax.swing.JButton();
+        combat = new javax.swing.JButton();
+        nextTurn = new javax.swing.JButton();
+
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setBackground(new java.awt.Color(255, 204, 255));
+
+        meet.setBackground(new java.awt.Color(255, 255, 204));
+        meet.setFont(new java.awt.Font("Skia", 0, 14)); // NOI18N
+        meet.setText("Conoce al monstruo");
+
+        combat.setFont(new java.awt.Font("Skia", 0, 14)); // NOI18N
+        combat.setText("¡¡A combatir!!");
+
+        nextTurn.setBackground(new java.awt.Color(255, 102, 102));
+        nextTurn.setFont(new java.awt.Font("Skia", 0, 14)); // NOI18N
+        nextTurn.setText("Siguiente turno");
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(25, 25, 25)
+                .addComponent(combat)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
+                .addComponent(nextTurn)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(meet)
+                .addGap(112, 112, 112))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap(213, Short.MAX_VALUE)
+                .addComponent(meet)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(nextTurn)
+                    .addComponent(combat))
+                .addGap(17, 17, 17))
         );
 
         pack();
@@ -44,7 +85,9 @@ public class NapakalakiView extends javax.swing.JFrame {
     public void showView(){
         this.setVisible(true);
     }
-
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton combat;
+    private javax.swing.JButton meet;
+    private javax.swing.JButton nextTurn;
     // End of variables declaration//GEN-END:variables
 }
