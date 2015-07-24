666b3217e06523e3f63c1f43366d59ff7d827f1d
montse <montse@192.168.1.104>
2015-07-24 18:29:59 +0200

    Comenzar el juego leyendo los nombres

---
+++ src/Model/Main.java
@@ -0,0 +1,25 @@
+package Model;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import GUI.*;
+
+/**
+ *
+ * @author Montse R. Zamorano
+ */
+
+public class Main {
+    public static void main(String[] args){
+        ArrayList <String> names = new ArrayList();    
+        Napakalaki napakalakiModel = Napakalaki.getInstance();
+        NapakalakiView napakalakiView = new NapakalakiView();
+        PlayerNamesCapture namesCapture=new PlayerNamesCapture(napakalakiView,true);
+        
+        names = namesCapture.getNames();
+        Dice.createInstance(napakalakiView);
+        napakalakiView.setNapakalaki(napakalakiModel);
+        napakalakiModel.initGame(names);
+        napakalakiView.showView();
+    }
+}
